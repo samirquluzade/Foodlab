7449dc3496a47815ebf3e6a8a0832fc96dd6574a
Samir Guluzada <samir.guluzada@sip.az>
2022-08-06 21:11:35 +0400

    Production

--- models/index.js
+++ models/index.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
+require('dotenv').config();
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-
 let sequelize;
 if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], config);
